bc5f35002d7fe90ddc5c829209bad93be035f0e3
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-27 02:20:06 +0530

    added all looping array function practice problem

---
+++ ArrayStatements/problem4.js
@@ -0,0 +1,12 @@
+let arr = [0, -1, 2, -3, 1, 2 ,-4];
+n = arr.length;
+console.log("Triplets with sum 0 are : ")
+for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+        for (let k = j + 1; k < n; k++) {
+            if (arr[i] + arr[j] + arr[k] == 0) {
+                console.log(arr[i] + "," + arr[j] + "," + arr[k] + "\n");
+            }
+        }
+    }
+}
